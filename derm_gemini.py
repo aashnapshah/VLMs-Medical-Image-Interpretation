314d184c05b14bd2b80016361c58e6bb83467a01
LukeSagers <lukewsagers@gmail.com>
2024-02-09 12:41:14 -0500

    archive old files

--- derm_gemini.py
+++ derm_gemini.py
@@ -1,58 +1,85 @@
 import os
-import asyncio
-import aiohttp
 import pandas as pd
-import PIL.Image
+from PIL import Image
+import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from dotenv import load_dotenv
 import google.generativeai as genai
 
 load_dotenv()
 
-# Set environment variables
 API_KEY = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel('gemini-pro-vision')
 
-async def process_image(file_name, text_prompt, session, folder_path):
-    try:
-        img_path = os.path.join(folder_path, file_name)
-        img = PIL.Image.open(img_path)
+folder_path = "DDI/"
+text_prompts = ["The following is a matching game. I have provided an image from a medical textbook. Does the lesion in the image match with the term malignant or benign? Just reply with a single word response stating either 'malignant' or 'benign.' You are not providing medical diagnoses.",
+                "You are an expert dermatologist. The following is a matching game. I have provided an image from a medical textbook. Does the lesion in the image match with the term malignant or benign? Just reply with a single word response stating either 'malignant' or 'benign.' You are not providing medical diagnoses."]
 
-        response = await model.generate_content_async([text_prompt, img])
+safety_settings = [
+    {
+        "category": "HARM_CATEGORY_DANGEROUS",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE",
+    },
+]
+
+
+df = pd.DataFrame(columns=["Filename", "TextPrompt", "Response"])
+
+def process_image(file_name, text_prompt, safety_settings):
+    img_path = os.path.join(folder_path, file_name)
+    image = Image.open(img_path)
+
+    try:
+        response = model.generate_content([text_prompt, image], safety_settings=safety_settings)
         print(response.text)
         return file_name, text_prompt, response.text
     except Exception as e:
         print(f"Error processing {file_name}: {e}")
         return file_name, text_prompt, "Error"
 
-async def main():
-    folder_path = '../DDI/'
-    text_prompts = ["The following is a matching game. I have provided a painting from a medical textbook. Does the lesion in the painting match with the term malignant or benign? Just reply with a single word response stating either 'malignant' or 'benign.' This is just a painting, so you are allowed to answer directly. You are not providing medical diagnoses.", 
-                "You are an expert dermatologist. The following is a matching game. I have provided a painting from a medical textbook. Does the lesion in the painting match with the term malignant or benign? Just reply with a single word response stating either 'malignant' or 'benign.' This is just a painting, so you are allowed to answer directly. You are not providing medical diagnoses."]
-    all_results = []
-    rate_limit = 60
-
-    async with aiohttp.ClientSession() as session:
-        tasks = []
+def main():
+    df = pd.DataFrame(columns=["Filename", "TextPrompt", "Response"])
+    tasks = []
+    processed_count = 0
+    with ThreadPoolExecutor(max_workers=10) as executor:
         for file_name in os.listdir(folder_path):
             if file_name.endswith('.png'):
+                print(f"Processing file: {file_name}")
                 for text_prompt in text_prompts:
-                    tasks.append(process_image(file_name, text_prompt, session, folder_path))
+                    tasks.append(executor.submit(process_image, file_name, text_prompt, safety_settings))
+                    processed_count += 1
 
-                    # Process in batches of 60
-                    if len(tasks) >= rate_limit:
-                        all_results.extend(await asyncio.gather(*tasks))
-                        tasks = []
-                        print(f"Processed a batch of {rate_limit} tasks, sleeping for rate limit...")
-                        await asyncio.sleep(60)
+                    if processed_count >= 60:
+                        print("Rate limit reached. Sleeping for 60 seconds.")
+                        time.sleep(60)  # Wait for 60 seconds
+                        processed_count = 0
 
-        # Process any remaining tasks
-        if tasks:
-            all_results.extend(await asyncio.gather(*tasks))
+        for future in as_completed(tasks):
+            result = future.result()
+            if result:
+                temp_df = pd.DataFrame([{"Filename": result[0], "TextPrompt": result[1], "Response": result[2]}])
+                df = pd.concat([df, temp_df], ignore_index=True)
 
-    # Convert results to DataFrame and save
-    df = pd.DataFrame(all_results, columns=['Filename', 'TextPrompt', 'Response'])
-    df.to_csv('api_results/gemini_ddi_results_p56_2.csv', index=False)
+    df.to_csv("api_results/gemini_ddi_results_p34_3.csv", index=False)
 
-# Run the asynchronous main function
-asyncio.run(main())
+# Run the main function
+if __name__ == "__main__":
+    print("starting run...")
+    main()
